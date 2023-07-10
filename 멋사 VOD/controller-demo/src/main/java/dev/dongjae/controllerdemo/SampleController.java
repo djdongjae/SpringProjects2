becbaed706bf8d4c029d0ae27f6d3816797ab998
djdongjae <isf1999@naver.com>
2023-07-10 21:01:56 +0900

    controller, RestController 연습

---
+++ 멋사 VOD/controller-demo/src/main/java/dev/dongjae/controllerdemo/SampleController.java
@@ -0,0 +1,30 @@
+package dev.dongjae.controllerdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SampleController {
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(name = "id", required = false, defaultValue = "") String id) {
+        logger.info("Path: Hello");
+        logger.info("Query Param id: " + id);
+        return "/hello.html";
+    }
+
+    @GetMapping(value = "/hello/{id}")
+    public String helloPath(@PathVariable("id") String id) {
+        logger.info("Path Variable is: " + id);
+        return "/hello.html";
+    }
+
+    @GetMapping("/get-profile")
+    public @ResponseBody SamplePayload getProfile() {
+        return new SamplePayload("Dongjae Oh", 25, "Student");
+    }
+
+}
