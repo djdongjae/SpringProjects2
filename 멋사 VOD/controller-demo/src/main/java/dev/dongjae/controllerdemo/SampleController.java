8eea11be9a88c9a390cb6e4674450107b37754d2
djdongjae <isf1999@naver.com>
2023-07-13 19:48:46 +0900

    HTML 템플릿 연습

--- 멋사 VOD/controller-demo/src/main/java/dev/dongjae/controllerdemo/SampleController.java
+++ 멋사 VOD/controller-demo/src/main/java/dev/dongjae/controllerdemo/SampleController.java
@@ -3,28 +3,47 @@ package dev.dongjae.controllerdemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("view")
 public class SampleController {
     private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam(name = "id", required = false, defaultValue = "") String id) {
-        logger.info("Path: Hello");
-        logger.info("Query Param id: " + id);
-        return "/hello.html";
+    @GetMapping("/sample-jsp")
+    public String sampleJsp(Model model) {
+        logger.info("in sample jsp");
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("Bradley", 29, "Accountant"));
+        profiles.add(new SamplePayload("Chris", 35, "Teacher"));
+
+        model.addAttribute("profiles", profiles);
+
+        return "view-jsp";
     }
 
-    @GetMapping(value = "/hello/{id}")
-    public String helloPath(@PathVariable("id") String id) {
-        logger.info("Path Variable is: " + id);
-        return "/hello.html";
-    }
+    @GetMapping("/sample-thyme")
+    public String sampleThyme(Model model) {
+        logger.info("in sample thyme");
 
-    @GetMapping("/get-profile")
-    public @ResponseBody SamplePayload getProfile() {
-        return new SamplePayload("Dongjae Oh", 25, "Student");
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("Bradley", 30, "Accountant"));
+        profiles.add(new SamplePayload("Chris", 35, "Teacher"));
+
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("profiles", profiles);
+//        modelAndView.setViewName("view-thyme");
+//        return modelAndView;
+
+        model.addAttribute("profiles", profiles);
+        return "view-thyme";
     }
 
 }
