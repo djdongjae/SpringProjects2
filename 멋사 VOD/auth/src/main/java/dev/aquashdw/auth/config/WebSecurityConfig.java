c92c6764275c3a693b1e07e08753db8de83f1fa7
djdongjae <isf1999@naver.com>
2023-07-18 20:36:19 +0900

    auth project userDetails 생성

---
+++ 멋사 VOD/auth/src/main/java/dev/aquashdw/auth/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package dev.aquashdw.auth.config;
+
+import dev.aquashdw.auth.infra.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    public WebSecurityConfig(@Autowired CustomUserDetailsService customUserDetailsService) {
+        userDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user1")
+//                .password(passwordEncoder().encode("user1pass"))
+//                .roles("USER");
+        auth.userDetailsService(this.userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests()
+                    .antMatchers("/home/**")
+                    .anonymous()
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/home")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/user/logout")
+                    .logoutSuccessUrl("/home")
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                    .permitAll()
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
