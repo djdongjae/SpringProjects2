f9f6f72d45090092875b20aca7d58d056b1326e8
djdongjae <isf1999@naver.com>
2023-09-02 22:00:05 +0900

    1 예제 만들기

---
+++ 김영한/01-스프링핵심원리_기본편/core_202309022140i/src/main/java/hello/core/Core202309022140iApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core202309022140iApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Core202309022140iApplication.class, args);
+    }
+
+}
