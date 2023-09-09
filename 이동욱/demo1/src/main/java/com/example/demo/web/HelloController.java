63d740dd521e0520753766bb932433ddd877b404
djdongjae <jk47wee@gmail.com>
2023-09-09 12:42:06 +0900

    helloDto API 추가

--- 이동욱/demo1/src/main/java/com/example/demo/web/HelloController.java
+++ 이동욱/demo1/src/main/java/com/example/demo/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-        @GetMapping("/hello")
-        public String hello() {
-            return "hello";
-        }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 
 }
