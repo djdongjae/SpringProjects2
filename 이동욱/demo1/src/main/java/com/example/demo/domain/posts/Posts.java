c13058aef713830b3dfbaa29555cf82329f596de
djdongjae <jk47wee@gmail.com>
2023-09-24 02:09:05 +0900

    JPA Auditing 기능 추가

--- 이동욱/demo1/src/main/java/com/example/demo/domain/posts/Posts.java
+++ 이동욱/demo1/src/main/java/com/example/demo/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.posts;
 
+import com.example.demo.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
