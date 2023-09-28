6bbbb4923662792b008edaffe4474b77667b4828
djdongjae <jk47wee@gmail.com>
2023-09-28 18:50:40 +0900

    폴더 위치 조정

--- 멋사 VOD/subway202307251023i/src/main/java/com/isf1999/subway202307251023i/repository/SubstationInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isf1999.subway202307251023i.repository;
-
-import com.isf1999.subway202307251023i.entity.SubstationInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubstationInfoRepository extends JpaRepository<SubstationInfo, Long> {
-}
--- 멋사 VOD/subway_2023_07_25_0922i/src/main/java/com/isf1999/subway_2023_07_25_0922i/repository/SubstationInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isf1999.subway_2023_07_25_0922i.repository;
-
-import com.isf1999.subway_2023_07_25_0922i.entity.SubstationInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubstationInfoRepository extends JpaRepository<SubstationInfo, Long> {
-}
