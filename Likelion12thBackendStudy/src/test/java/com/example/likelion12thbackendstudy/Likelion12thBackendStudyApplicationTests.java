bf7f90e36cad2af0bf258226c1875c199abc206c
djdongjae <jk47wee@gmail.com>
2023-12-31 21:43:58 +0900

    멋사 백엔드 스터디 초기 세팅

---
+++ Likelion12thBackendStudy/src/test/java/com/example/likelion12thbackendstudy/Likelion12thBackendStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion12thbackendstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion12thBackendStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
